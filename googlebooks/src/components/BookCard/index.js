43dbc40fbb48b72d4b427afa0d146e482f687eae
Curtis Jochen <curtis.jochen@gmail.com>
2019-12-25 00:16:31 -0500

    started styling the BookCard and handling information from the API request

--- googlebooks/src/components/BookCard/index.js
+++ googlebooks/src/components/BookCard/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 
-const BookCard = ({book}) => {
+const BookCard = ({ book }) => {
 
     return (
-        <div>
-            <h3>Title:</h3>
-            <h3>Author:</h3>
-            <h3>Image:</h3>
-            <h3>Description:</h3>
+        <div className='container'>
+            <div className='row'>
+                <div className='title col-8'>{book.volumeInfo.title}</div>
+                <div className='save col-2'>
+                    <button>Save</button>
+                </div>
+                <div className='view col-2'>
+                    <button>View</button>
+                </div>
+            </div>
+            <div className='row'>
+                <div className='author'>{book.volumeInfo.authors}</div>
+            </div>
         </div>
     )
 }
