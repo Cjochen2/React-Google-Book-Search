4a06870feaac306ee35b0a3ae08660f5917a3640
Curtis Jochen <curtis.jochen@gmail.com>
2019-12-26 13:47:20 -0500

    Found a very strange bug that I cannot fix involving redering images. If you search the word TEST it will throw a "cannot read property of undefined error" not sure if the bug is limited to just this search param but so far all other querys work as intended without any error. Images render with the search along with the description of the books.Styling still needs to be completed. Other wise the API call and render is finished. Now need to hook up DB.

--- googlebooks/src/components/BookCard/index.js
+++ googlebooks/src/components/BookCard/index.js
@@ -2,10 +2,12 @@ import React from 'react'
 
 const BookCard = ({ book }) => {
 
+    let bookInfo = book.volumeInfo
+
     return (
-        <div className='container'>
+        <div className='bookCard container mt-3 mb-5'>
             <div className='row'>
-                <div className='title col-8'>{book.volumeInfo.title}</div>
+                <div className='title col-8 text-left pl-0'>{bookInfo.title}</div>
                 <div className='save col-2'>
                     <button>Save</button>
                 </div>
@@ -14,7 +16,15 @@ const BookCard = ({ book }) => {
                 </div>
             </div>
             <div className='row'>
-                <div className='author'>{book.volumeInfo.authors}</div>
+                <div className='author'>Written By: {bookInfo.authors}</div>
+            </div>
+            <div className='row'>
+                <div className='col-4 text-center'>
+                <img src={bookInfo.imageLinks.thumbnail} alt={bookInfo.title} className="img-fluid img-thumbnail" />
+                </div>
+                <div className='col-8'>
+                    {bookInfo.description}
+                </div>
             </div>
         </div>
     )
